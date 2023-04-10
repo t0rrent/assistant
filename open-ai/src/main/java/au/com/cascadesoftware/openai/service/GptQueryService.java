b86e53e7e84ca729eeb0c4aa9b9d6228777c0624
Kyle <kyle.avery3366@gmail.com>
2023-04-11 01:18:59 +1000

    fix spelling error

--- open-ai/src/main/java/au/com/cascadesoftware/openai/service/GptQueryService.java
+++ open-ai/src/main/java/au/com/cascadesoftware/openai/service/GptQueryService.java
@@ -8,6 +8,6 @@ public interface GptQueryService {
 
 	Conversation message(Conversation conversation);
 
-	void messageAsync(Conversation conversation, Consumer<Conversation> callbackk);
+	void messageAsync(Conversation conversation, Consumer<Conversation> callback);
 
 }
